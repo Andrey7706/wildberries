1c95c27bb4c8d45a6385911e20ad4d57f1f3065c
Andrey <Andrey7706@gmail.com>
2026-05-20 23:01:04 +0300

    feat: add goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,24 +1,72 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+
+    const renderGoods = () => {
+        const goods = JSON.parse(localStorage.getItem('goods'));
+        if (!goods?.length) return;
+
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `
+
+            goodsContainer.append(goodBlock)
+        })
+    }
     
-    const getData = () => {
+    const getData = (value, category) => {
         fetch('https://wildberries-1628b-default-rtdb.firebaseio.com/db.json')
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
-                localStorage.setItem('goods', JSON.stringify(data));
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (!window.location.pathname.endsWith('goods.html')) {
+                    window.location.href = "goods.html"
+                } else {
+                    renderGoods()
+                }
             });
+    }
+
+    const viewAllBtn = document.querySelector('.more');
+
+    if (viewAllBtn) {
+        viewAllBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            getData();
+        });
     }
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
-            getData();
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getData(linkValue, category);
         });
     });
 
-    const goods = JSON.parse(localStorage.getItem('goods'));
-    console.log(goods);
+    if (localStorage.getItem('goods') && window.location.pathname.endsWith('goods.html')) {
+        renderGoods()
+    }
 
 }
 
